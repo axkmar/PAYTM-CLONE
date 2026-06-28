68b6ef44f3f1315a367610be94b51997b81c430d
yellowrevolver22 <akshitk275@gmail.com>
2026-06-28 17:59:11 +0530

    add router-frontend,template-Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes,Route } from "react-router-dom"
+import Signup from "./pages/Signup/Signup"
+import Signin from "./pages/Signin/Signin"
+import Send from "./pages/Send/Send"
+import Dashboard from "./pages/dashboard/Dashboard"
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <Routes>
+      <Route path="/signup" element={<Signup></Signup>}/>
+      <Route path="/signin" element={<Signin></Signin>}/>
+      <Route path="/dashboard" element={<Dashboard></Dashboard>}/>
+      <Route path="/send" element={<Send></Send>} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,5 @@
+export default function Dashboard(){
+  return <div>
+    THIS IS DASHBOARD
+  </div>
+}
