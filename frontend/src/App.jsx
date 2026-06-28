b92254e27c5e8eef848f2b6f67ffbb87c624fe12
yellowrevolver22 <akshitk275@gmail.com>
2026-06-28 20:11:54 +0530

    add signin,signup components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes,Route } from "react-router-dom"
-import Signup from "./pages/Signup/Signup"
-import Signin from "./pages/Signin/Signin"
-import Send from "./pages/Send/Send"
-import Dashboard from "./pages/dashboard/Dashboard"
+import {Signup} from "./pages/Signup"
+import {Signin} from "./pages/Signin"
+import SendMoney from "./pages/SendMoney"
+import Dashboard from "./pages/dashboard"
 
 function App() {
 
@@ -11,7 +11,7 @@ function App() {
       <Route path="/signup" element={<Signup></Signup>}/>
       <Route path="/signin" element={<Signin></Signin>}/>
       <Route path="/dashboard" element={<Dashboard></Dashboard>}/>
-      <Route path="/send" element={<Send></Send>} />
+      <Route path="/send" element={<SendMoney></SendMoney>} />
     </Routes>
   )
 }
